184e435ec787eb2a7e2185a5957ae7de8f747f5b
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-01-30 21:51:27 +0200

    fix problem with pixabay.api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,8 @@
 const API_KEY = '48293872-b2eb1e8e6b9d94230c1481ee9';
 const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
 
-export function serviceImages(question) {
+export async function serviceImages(question, page = 1) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: question,
@@ -12,10 +13,10 @@ export function serviceImages(question) {
     page,
   });
 
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  try {
+    const response = await axios.get(`${BASE_URL}?${params}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
 }
